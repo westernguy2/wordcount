af8892eaa82f841a8cf008b4f2089a83a90b4421
westernguy2 <kunalagarwal2399@gmail.com>
2018-08-21 15:25:22 -0700

    finished word project

---
+++ wordcount/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+def home(request):
+	return render(request, 'home.html')
+def count(request):
+	fulltext = request.GET['fulltext']
+	wordlist = fulltext.split()
+	worddictionary = {}
+	for word in wordlist:
+		if word in worddictionary:
+			worddictionary[word] += 1
+		else:
+			worddictionary[word] = 1
+	sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True)
+	return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist), 'worddictionary': sortedwords})
+def aboutus(request):
+	return render(request, 'aboutus.html')
